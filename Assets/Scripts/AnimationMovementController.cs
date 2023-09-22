ab68da5cbe2d243258906daf0492778376988a19
Jonas Sun <jonassunhk@gmail.com>
2023-09-22 10:02:55 +0800

    Character+Camera Control
    Added moving character and cinemachine camera control

--- Assets/Scripts/AnimationMovementController.cs
+++ Assets/Scripts/AnimationMovementController.cs
@@ -3,37 +3,84 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class AnimationMovementController : MonoBehaviour
 {
     PlayerInput playerInput;
     CharacterController characterController;
 
+    int isRunningHash;
+    int isJumpingHash;
+
     Vector2 currentMovementInput;
     Vector3 currentMovement;
     bool isMovementPressed;
+    Animator animator;
+    float rotationFactorPerFrame = 10.0f;
+    float runSpeed = 7.0f;
+    
 
     private void Awake()
     {
         Debug.Log("here");
         playerInput = new PlayerInput();
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
 
-        playerInput.CharacterControls.Move.started += context => {
+        isRunningHash = Animator.StringToHash("isRunning");
+        isJumpingHash = Animator.StringToHash("isJumping");
 
-            Debug.Log("Key Pressed");
-            currentMovementInput = context.ReadValue<Vector2>();
-            currentMovement.x = currentMovementInput.x;
-            currentMovement.z = currentMovementInput.y;
-            isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+        playerInput.CharacterControls.Move.started += onMovementInput;
+        playerInput.CharacterControls.Move.canceled += onMovementInput;
+        playerInput.CharacterControls.Move.performed += onMovementInput;
+    }
 
-        };
+    void onMovementInput (InputAction.CallbackContext context)
+    {
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.z = currentMovementInput.y;
+        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
 
+    }
+    void handleRotation()
+    {
+        Vector3 positionToLookAt;
+
+        positionToLookAt.x = currentMovement.x;
+        positionToLookAt.y = 0.0f;
+        positionToLookAt.z = currentMovement.z;
+
+        Quaternion currentRotation = transform.rotation;
+
+        if (isMovementPressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+        }
+    }
+
+    void handleAnimation()
+    {
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool isJumping = animator.GetBool(isJumpingHash);
+
+        if (isMovementPressed && !isRunning) {
+            animator.SetBool(isRunningHash, true);
+        }
+        
+        else if (!isMovementPressed && isRunning)
+        {
+            animator.SetBool(isRunningHash, false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        characterController.Move(currentMovement);
+        handleRotation();
+        handleAnimation();
+        characterController.Move(currentMovement * Time.deltaTime * runSpeed);
     }
 
     private void OnEnable()
